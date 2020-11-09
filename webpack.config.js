9091332521b04bf1e7fa66f2c2b1829c03193ff7
최경용 <jingyong9111@gamil.com>
2020-11-09 16:58:04 +0900

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 // 노드의 path 모듈
 const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -30,17 +35,53 @@ module.exports = {
       },
       {
         test: /\.css$/, // .css 확장자로 끝나는 모든 파일
-        use: ["style-loader", "css-loader"] // css-loader를 적용한다
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader // 프로덕션 환경
+            : "style-loader", // 개발 환경
+          // "style-loader",
+          "css-loader"
+        ] // css-loader를 적용한다
       },
       {
         test: /\.(png|jpg|gif|svg)$/, // .png 확장자로 마치는 모든 파일
         loader: "file-loader", // 파일 로더를 적용한다
         options: {
-          publicPath: "./dist/", // prefix를 아웃풋 경로로 지정
+          publicPath: "./", // prefix를 아웃풋 경로로 지정
           name: "[name].[ext]?[hash]", // 파일명 형식
           limit: 20000 // 20kb 미만 파일만 data url로 처리
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+      빌드 날짜: ${new Date().toLocaleString()}
+      유저 : ${childProcess.execSync("git config user.name")}
+      커밋:${childProcess.execSync("git rev-parse --short HEAD")}`
+    }),
+    new webpack.DefinePlugin({
+      TWO: "1+1",
+      VERSION: JSON.stringify("v.1.2.3")
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // 템플릿 경로를 지정
+      templateParameters: {
+        // 템플릿에 주입할 파라매터 변수 지정
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true // 주석 제거
+            }
+          : false
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : [])
+  ]
 };
